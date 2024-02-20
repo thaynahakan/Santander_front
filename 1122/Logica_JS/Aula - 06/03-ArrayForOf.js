c97b8a9c6235ff12d254ea4723e832d4e2c1e6a6
Thayna Bittencourt Baima <131307382+thaynahakan@users.noreply.github.com>
2024-02-20 07:27:42 -0300

    exercicio 3 completo

--- 1122/Logica_JS/Aula - 06/03-ArrayForOf.js
+++ 1122/Logica_JS/Aula - 06/03-ArrayForOf.js
@@ -1,3 +1,11 @@
 /*Crie um array com pelo menos 5 nomes de frutas.
 Utilize um loop for-of para imprimir no console cada nome de fruta.
  */
+
+let frutas = [ 'Banana', 'Limão', 'Maçã', 'Pêssego', 'Melancia'];
+
+for(let fruta of frutas){
+    console.log(`Nome: ${fruta}`)
+}
+
+//Não funciona se colocar sendo um objeto {}, tem que ser um array [] pra desempacotar.

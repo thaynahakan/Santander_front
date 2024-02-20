0d3ec3c3be30ab8ffd6367831949080328b9095c
Thayna Bittencourt Baima <131307382+thaynahakan@users.noreply.github.com>
2024-02-20 07:38:56 -0300

    att

--- 1122/Logica_JS/Aula - 06/04-TabuadaFor.js
+++ 1122/Logica_JS/Aula - 06/04-TabuadaFor.js
@@ -3,3 +3,9 @@ for.
 Saída: N X 1 = 10 
 */
 
+let numero = prompt(parseInt("Insira qual tabuada você deseja ver:"));
+
+for(let i = 1; i <= 10; i++){
+    conta = i * numero
+    console.log(`${numero} X ${i} = ${conta}`)
+}
--- 1122/Logica_JS/Aula - 06/05-ForInSomaVal.js
+++ 1122/Logica_JS/Aula - 06/05-ForInSomaVal.js
@@ -2,3 +2,13 @@
 calcular e imprimir a soma desses valores.
 { valor1: 10, valor2: 20, valor3: 30, valor4: 40 }
  */
+
+let valores = {valor1: 10, valor2: 20, valor3: 30, valor4: 40};
+
+let soma = 0;
+
+for (let chave in valores) {
+    soma += valores[chave];
+}
+
+console.log(`A soma dos valores é: ${soma}`);

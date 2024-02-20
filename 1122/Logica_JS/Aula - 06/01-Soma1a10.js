4c99d66ae8ed8ded3162ba62d52605a76f05d8b9
Thayna Bittencourt Baima <131307382+thaynahakan@users.noreply.github.com>
2024-02-20 07:08:01 -0300

    finalizado ex 1

--- 1122/Logica_JS/Aula - 06/01-Soma1a10.js
+++ 1122/Logica_JS/Aula - 06/01-Soma1a10.js
@@ -2,3 +2,12 @@
 imprima o resultado. 
 (1+2=3+3=6+4=10+5=15)
  */
+let soma = 0;
+
+// Loop for para somar os números de 1 a 10
+for (let i = 1; i <= 10; i++) {
+    soma += i;
+}
+
+// Imprimir o resultado
+console.log("A soma dos números de 1 a 10 é: " + soma);

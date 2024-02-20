3d7e114c6741c57b5c77d33a0b3d7b7235c91f4e
Thayna Bittencourt Baima <131307382+thaynahakan@users.noreply.github.com>
2024-02-19 21:44:13 -0300

    feat:

--- 1122/Logica_JS/Aula - 06/08-ContarVogaisFor.js
+++ 1122/Logica_JS/Aula - 06/08-ContarVogaisFor.js
@@ -2,3 +2,16 @@
 [1,2,3,4,5,6,7,8,9,10] 
 Achei o número 7
  */
+function contarVogais(palavra) {
+    const vogais = ['a', 'e', 'i', 'o', 'u'];
+    let contador = 0;
+    for (let i = 0; i < palavra.length; i++) {
+        if (vogais.includes(palavra[i])) {
+            contador++; 
+        }
+    }
+    return contador;
+   }
+   const palavra = prompt("Digite uma palavra:");
+   const quantidadeVogais = contarVogais(palavra);
+   console.log(`A palavra '${palavra}' possui ${quantidadeVogais} vogais.`);
---
+++ 1122/Logica_JS/Aula - 08/01 - Teste-de-Logica.js
@@ -0,0 +1,15 @@
+/* 1 - Dados cinco números inteiros positivos, encontre os valores 
+mínimo e máximo que podem ser calculados somando exatamente quatro 
+dos cinco números inteiros. Em seguida, imprima os respectivos 
+valores mínimo e máximo como uma única linha de dois inteiros 
+separados por espaço.
+Exemplo: 
+arr - [1,3,5,7,9]
+a soma mínima é: 1 + 3 + 5 + 7 = 16
+a soma máxima é: 3 + 5 + 7 + 9 = 24
+Saída: [16,24]
+Teste: 
+arr = [5,1,6,3,9] => Saída => [15, 23]
+arr = [9,3,3,5,8] => Saída => [19, 25]
+ */
+

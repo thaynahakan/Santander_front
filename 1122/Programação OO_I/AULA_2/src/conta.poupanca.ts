3cb5595c332a2215d995abf10f6b0d182e87b940
Thayna Bittencourt Baima <131307382+thaynahakan@users.noreply.github.com>
2024-03-06 22:35:03 -0300

    att

--- 1122/Programação OO_I/AULA_2/src/conta.poupanca.ts
+++
@@ -1,9 +0,0 @@
-class Poupança extends ContaGeral {
-  taxaDeRendimento: number;
-  //depositar
-  //consultar saldo
-  //sacar
-  //consultar extrato
-  //taxa de rendimento
-  //exibir a taxa de rendimento
-}
